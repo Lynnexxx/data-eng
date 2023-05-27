1b295b6cd4206bba7248071a261750bf99bcb136
lynnexxx <essenahounagbaholou@gmail.com>
2023-05-27 11:37:37 +0200

    producer-updating

--- src/main/scala/com/peaceland/kafka/processing/KafkaProducerApp.scala
+++ src/main/scala/com/peaceland/kafka/processing/KafkaProducerApp.scala
@@ -1,80 +1,79 @@
-
 package com.peaceland.kafka.processing
 
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.streaming._
 import com.peaceland.utils._
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord, RecordMetadata}
-import org.apache.kafka.common.serialization.StringSerializer
-import play.api.libs.json.Json
 
-import java.util.{Properties, Timer, TimerTask}
-
-object KafkaProducerApp //extends App
-{
-  //Création et configuration du producer
-  val props: Properties = new Properties()
-  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-  props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
-  props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
-  props.put("acks","all")
-
-  val producer: KafkaProducer[String, String] = new KafkaProducer[String, String](props)// producer instance
-
-  val nbDrones =10 //Number of drones in activity
-  val topiC ="peaceLand"
-  def droneData(): Unit = {
-    println("Sending data to topic peaceLand")
-    val drones = Drone.generateDrone(nbDrones)
-    drones.foreach(drone => droneDataToKafka(drone))
-  }
-
-  def droneDataToKafka(drone: Drone): Unit = {
-    implicit val reportImplicitWrites = Json.writes[Report]
-    println(reportImplicitWrites)
-    val droneReport = Json.toJson(Report.generateReport(drone)) //Converting repport in JSON format
-    println(droneReport)
-    val record: ProducerRecord[String, String] = new ProducerRecord[String, String](topiC, drone.droneId.toString, droneReport.toString)
-    producer.send(record, (recordMetadata: RecordMetadata, exception: Exception) => {
-      if (exception != null) {
-        exception.printStackTrace()
-      }
-      else {
-        println(s"Metadata about the sent record : $recordMetadata")
-      }
-    })
-    println("sending data via kafka" + droneReport.toString())
-
-    producer.flush()
-  }
-
-
+object KafkaProducerApp {
   def main(args: Array[String]): Unit = {
-    println("Starting producer ............")
-    droneData()
-    val timer = new Timer()
-    val timerTask = new TimerTask { //Difining the timer task
-      override def run(): Unit = { //override is used to redifine the existing function run()
-        droneData()
-      }
-    }
-    timer.schedule(timerTask, 50, 30000)
-    /*
-    * The first repport willl be sent after 50 milisecond
-    * There is 5000 ms (5s) between the sending of each repport
-    * */
-  }
-  //Envoi d'un message
-  /*val key = "akey"
-  val value = "my second message"
-  val topic="text_topic"
+    // Create the Spark session
+    val spark = SparkSession
+      .builder()
+      .appName("Peacland_storage")
+      .master("local[*]")
+      .config("some.config.option", "some-value")
+      .getOrCreate()
 
-  val record: ProducerRecord[String, String] = new ProducerRecord[String, String](topic, key, value)
-  producer.send(record, (recordMetadata: RecordMetadata, exception: Exception) => {
-    if (exception != null) {
-      exception.printStackTrace()
-    }
-    else {
-      println(s"Metadata about the sent record : $recordMetadata")
-    }
-  })*/
+    // Use the case class Report to define the schema of the Spark DataFrame
+    val mySchema = Encoders.product[Report].schema
 
-}
+    // Define the Kafka topic and bootstrap server
+    val topic = "peaceLand"
+    val bootstrapServer = "localhost:9092"
+
+    // Read data from the Kafka stream
+    val kafkaToDf: DataFrame = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", bootstrapServer)
+      .option("subscribe", topic)
+      .option("startingOffsets", "earliest")
+      .load()
+
+    // Transform the retrieved values
+    val df = kafkaToDf
+      .select(from_json(col("value").cast("string"), mySchema).as("Data_report"))
+      .select("Data_report.*")
+      .withColumnRenamed("droneId", "id")
+      .withColumn("id", col("id").cast(StringType))
+
+    // Configure Azure Cosmos DB
+    val cosmosEndpoint = "https://data-ing.documents.azure.com:443/"
+    val cosmosMasterKey = "1s9ljz9iHzSINDtnGYiCrJOHy9oEzJiSAF9LqPbD7yyEGN1FolSyFNeM4SMgXtkOVZXHWHKQdWLKACDbc9j5LQ==" // Replace with your key
+    val cosmosDatabaseName = "sampleDB"
+    val cosmosContainerName = "sampleContainer"
+
+    val cfg = Map(
+      "spark.cosmos.accountEndpoint" -> cosmosEndpoint,
+      "spark.cosmos.accountKey" -> cosmosMasterKey,
+      "spark.cosmos.database" -> cosmosDatabaseName,
+      "spark.cosmos.container" -> cosmosContainerName
+    )
+
+    // Configure the Cosmos Catalog API to be used
+    spark.conf.set(s"spark.sql.catalog.cosmosCatalog", "com.azure.cosmos.spark.CosmosCatalog")
+    spark.conf.set(s"spark.sql.catalog.cosmosCatalog.spark.cosmos.accountEndpoint", cosmosEndpoint)
+    spark.conf.set(s"spark.sql.catalog.cosmosCatalog.spark.cosmos.accountKey", cosmosMasterKey)
+
+    // Write the initial DataFrame to Cosmos DB
+    spark.createDataFrame(Seq(("cat-alive1", "Schrodinger cat", 2, true), ("cat-dead1", "Schrodinger cat", 2, false)))
+      .toDF("id", "name", "age", "isAlive")
+      .write
+      .format("cosmos.oltp")
+      .options(cfg)
+      .mode("APPEND")
+      .save()
+
+    // Write the streaming data to Cosmos DB
+    val query = df
+      .writeStream
+      .format("cosmos.oltp")
+      .options(cfg)
+      .option("checkpointLocation", "./checkpoints/reports")
+      .outputMode(OutputMode.Append())
+      .start()
+
+    query.awaitTermination()
+
